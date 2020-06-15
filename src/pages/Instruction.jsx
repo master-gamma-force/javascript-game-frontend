6f440c21c3009434c74a8e365e9a332dea6acb15
David Behar <davbelom@gmail.com>
2020-06-15 10:44:31 -0500

    (Instruction): Add Instruction component and dynamic markdown import

---
+++ src/pages/Instruction.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import sitemap from '../content/sitemap.json'
+import Markdown from '../components/Markdown'
+import useDynamicMarkdownImport from '../hooks/useDynamicMarkdownImport'
+
+const Instruction = () => {
+  const { moduleId, levelId } = useParams()
+  const module = sitemap
+    .filter((module) => module.id === moduleId)
+    .reduce((module) => module)
+  const level = module.levels
+    .filter((level) => level.id === parseInt(levelId, 10))
+    .reduce((level) => level)
+  const { contentPath } = level
+  const [text] = useDynamicMarkdownImport(contentPath)
+
+  return (
+    <Markdown text={text} />
+  )
+}
+
+export default Instruction
