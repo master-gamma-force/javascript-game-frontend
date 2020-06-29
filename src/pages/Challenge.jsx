6c9341de4b222c6df3a2062d9e3d6bca0bf92eb6
Gerardo Marquez Carmona <gerardomarquezcarmona@MacBook-Air-de-Gerardo.local>
2020-06-29 13:26:02 -0500

    cambios a los botones

--- src/pages/Challenge.jsx
+++ src/pages/Challenge.jsx
@@ -26,6 +26,7 @@ const handleResponseTest = (event, { tests, setTests, setAllowNext }) => {
     return test
   }))
   const allowNext = tests.reduce((pass, test) => pass && test.status === 'valid', true)
+  console.log(allowNext)
   setAllowNext(allowNext)
 }
 
@@ -60,7 +61,7 @@ const Challenge = () => {
       </div>
       <div className="Challenge-Tests">
         {
-          tests.map((test) => <Test status={test.status} text={test.description} />)
+          tests.map((test) => <Test status={test.status} text={test.description} key={test.type} />)
         }
       </div>
       <div className="Challenge-Editor">
@@ -75,7 +76,18 @@ const Challenge = () => {
               to={`/${moduleObj.id}/${level.id}/instructions`}
               small
             >
-              {'<-'}
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 443.52 443.52"
+                transform="rotate(180)"
+              >
+                <g>
+                  <g>
+                    <path xmlns="http://www.w3.org/2000/svg" d="M352.025,196.712L165.884,10.848C159.029,3.615,150.469,0,140.187,0c-10.282,0-18.842,3.619-25.697,10.848L92.792,32.264   c-7.044,7.043-10.566,15.604-10.566,25.692c0,9.897,3.521,18.56,10.566,25.981l138.753,138.473L92.786,361.168   c-7.042,7.043-10.564,15.604-10.564,25.693c0,9.896,3.521,18.562,10.564,25.98l21.7,21.413   c7.043,7.043,15.612,10.564,25.697,10.564c10.089,0,18.656-3.521,25.697-10.564l186.145-185.864   c7.046-7.423,10.571-16.084,10.571-25.981C362.597,212.321,359.071,203.755,352.025,196.712z"/>
+                  </g>
+                </g>
+              </svg>
             </Button>
 
             <Button
@@ -83,12 +95,26 @@ const Challenge = () => {
             >
               Correr Pruebas
             </Button>
-            <Button
-              to={`/${moduleObj.id}/${level.nextLevelId}/instructions`}
-              disabled={allowNext}
-            >
-              {'->'}
-            </Button>
+            {
+              level.nextLevelId ? (
+                <Button
+                  className="button-next"
+                  to={`/${moduleObj.id}/${level.nextLevelId}/instructions`}
+                >
+                  <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 443.52 443.52"
+                  >
+                    <g>
+                      <g>
+                        <path xmlns="http://www.w3.org/2000/svg" d="M352.025,196.712L165.884,10.848C159.029,3.615,150.469,0,140.187,0c-10.282,0-18.842,3.619-25.697,10.848L92.792,32.264   c-7.044,7.043-10.566,15.604-10.566,25.692c0,9.897,3.521,18.56,10.566,25.981l138.753,138.473L92.786,361.168   c-7.042,7.043-10.564,15.604-10.564,25.693c0,9.896,3.521,18.562,10.564,25.98l21.7,21.413   c7.043,7.043,15.612,10.564,25.697,10.564c10.089,0,18.656-3.521,25.697-10.564l186.145-185.864   c7.046-7.423,10.571-16.084,10.571-25.981C362.597,212.321,359.071,203.755,352.025,196.712z"/>
+                      </g>
+                    </g>
+                  </svg>
+                </Button>
+              ) : null
+            }
           </div>
         </div>
       </div>
